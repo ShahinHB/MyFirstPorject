1b9fa6fd89c184fb635f27090c7968da453864ee
ShahinHB <72246220+ShahinHB@users.noreply.github.com>
2020-11-20 15:11:32 +0400

    Created Interface

---
+++ MyFirst/MyFirst/Infrastructure/Interfaces/IMarketable.cs
@@ -0,0 +1,25 @@
+﻿using MyFirstProject.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirst.Infrastructure.Interfaces
+{
+    public interface IMarketable
+    {
+        List<Sale> Sales { get;}
+
+        List<Sale> Products { get; }
+        void AddSale(string Name);
+        void RemoveProduct(int Sale);
+        double TotalSaleDatebyDate(DateTime StartDate, DateTime EndDate);
+        double TotalSaleWithDate(DateTime Date);
+        double TotalSalWithPrice(double StartPrice, double EndPrice);
+        double 
+
+
+
+
+
+    }
+}
