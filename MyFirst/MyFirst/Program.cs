51f547c18145386bb6d79276825d410c28136596
ShahinHB <72246220+ShahinHB@users.noreply.github.com>
2020-11-20 12:40:13 +0400

    Project Created

---
+++ MyFirst/MyFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
